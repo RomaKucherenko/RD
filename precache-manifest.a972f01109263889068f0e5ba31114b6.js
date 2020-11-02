a44489d5a9cbf0e0fee1a416f7edbe666f44ea26
RomaKucherenko <clough@list.ru>
2020-11-02 12:24:01 +0300

    Updates

---
+++ precache-manifest.a972f01109263889068f0e5ba31114b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f72d075a1828f1769b0c54e724ea59c",
+    "url": "/RD/index.html"
+  },
+  {
+    "revision": "6cde07ea1eac0889be7b",
+    "url": "/RD/static/css/3.093ac377.chunk.css"
+  },
+  {
+    "revision": "9450971bcab31f5d2c41",
+    "url": "/RD/static/css/main.13eb1c4b.chunk.css"
+  },
+  {
+    "revision": "095d4e764a1b99e51874",
+    "url": "/RD/static/js/2.5c7c39f3.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/RD/static/js/2.5c7c39f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cde07ea1eac0889be7b",
+    "url": "/RD/static/js/3.caddb13d.chunk.js"
+  },
+  {
+    "revision": "9450971bcab31f5d2c41",
+    "url": "/RD/static/js/main.6f3d0b81.chunk.js"
+  },
+  {
+    "revision": "6163f13476706ae4b8e79d15edab08eb",
+    "url": "/RD/static/js/main.6f3d0b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74eea14d10ad19d6c5c4",
+    "url": "/RD/static/js/runtime-main.377e1ca5.js"
+  },
+  {
+    "revision": "7956c94df432cd345923ae5c977aef04",
+    "url": "/RD/static/media/45.7956c94d.gif"
+  },
+  {
+    "revision": "5ad0ef7eb163bd2df22a6d16c05c9090",
+    "url": "/RD/static/media/RD.5ad0ef7e.jpg"
+  }
+]);
